e99e1dfce5d0411097c36fa62428fd1a93994704
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-02-03 21:48:41 +0530

    Add files via upload

---
+++ add 10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+   int size;
+   scanf("%d",&size);
+   int arr[size];
+   for(int i=0;i<size;i++){
+    scanf("%d",&arr[i]);
+   }
+   int size_of =size/2;
+   for(int i=0;i<size;i++){
+        if(arr[i]>size_of){
+            printf("%d",arr[i]);
+        }
+   }
+    
+    return 0;
+}
