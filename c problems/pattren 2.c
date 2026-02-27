dce8eaff5410eeafbd47e79f01a430134f7ad734
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-02-27 12:01:47 +0530

    Update pattren 2.c

--- c problems/pattren 2.c
+++ c problems/pattren 2.c
@@ -2,9 +2,9 @@
 int main(){
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-              printf("* ");
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<i+1;j++){
+              printf("*");
         }
         printf("\n");
     }
