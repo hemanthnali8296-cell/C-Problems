80284dcfeb23a214185575919722493adda10a08
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-02-28 15:26:40 +0530

    Add files via upload

---
+++ c problems/pattren 9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int space=1;space<=n-i;space++){
+            printf("  ");
+        }for(int j=1;j<=i*2-1;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }for(int i=n-1;i>=1;i--){
+        for(int space=1;space<=n-i;space++){
+            printf("  ");
+        }for(int j=1;j<=i*2-1;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
