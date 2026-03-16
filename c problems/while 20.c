f21152273d1eddfc767d91d2b1875886ad3f07f1
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-03-16 21:05:48 +0530

    Add files via upload

---
+++ c problems/while 20.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int h;
+    int i=0;
+    while(i<n){
+        scanf("%d",&h);
+    }
+    i++
+    printf("%d",h);
+    return 0;
+}
