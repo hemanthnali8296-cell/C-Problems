5a1f7a50cb8dd4a8aa5623d75e39587f26096c26
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-03-20 21:22:53 +0530

    Add files via upload

---
+++ c problems/while 25.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int arr[N];
+    int i = 0;
+    while (i < N) {
+        scanf("%d", &arr[i]);
+        i++;
+    }
+    int highValueCount = 0;
+    int consecutive = 0;
+    int fraudAttempt = -1;
+    i = 0;
+    while (i < N) {
+        if (arr[i] >= 50000) {
+            highValueCount++;
+            consecutive++;
+            if (consecutive == 3 && fraudAttempt == -1) {
+                fraudAttempt = i + 1; 
+            }
+        } else {
+            consecutive = 0; 
+        }
+        i++;
+    }
+    if (fraudAttempt != -1) {
+        printf("Fraud Triggered At Attempt: %d\n", fraudAttempt);
+    } else {
+        printf("Fraud Triggered At Attempt: Not Triggered\n");
+    }
+    printf("High-Value Transactions: %d\n", highValueCount);
+    return 0;
+}
