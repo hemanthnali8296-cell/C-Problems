a0dbc8abf595887fdbe4bd0b6d9da5c42a18fe74
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-05-18 20:32:18 +0530

    Add files via upload

---
+++ c problems/array10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+    int data[R][C];
+    for(int i = 0; i < R; i++) {
+        for(int j = 0; j < C; j++) {
+            scanf("%d", &data[i][j]);
+        }
+    }
+    long long maxProduct;
+    int maxRow = 0;
+    maxProduct = 1;
+    for(int j = 0; j < C; j++) {
+        maxProduct *= data[0][j];
+    }
+    for(int i = 1; i < R; i++) {
+        long long product = 1;
+        for(int j = 0; j < C; j++) {
+            product *= data[i][j];
+        }
+        if(product > maxProduct) {
+            maxProduct = product;
+            maxRow = i;
+        }
+    }
+    printf("%d", maxRow);
+    return 0;
+}
