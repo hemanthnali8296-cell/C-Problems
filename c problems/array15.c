f946ed2f591621bf823f2c60095542e67529d420
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-05-23 16:37:30 +0530

    Add files via upload

---
+++ c problems/array15.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int isPrime(int n) {
+    if (n <= 1)
+        return 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+    return 1;
+}
+int main() {
+    int R, C;
+    scanf("%d %d", &R, &C);
+    int matrix[R][C];
+    int count = 0;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            scanf("%d", &matrix[i][j]);
+
+            if (isPrime(matrix[i][j])) {
+                count++;
+            }
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
