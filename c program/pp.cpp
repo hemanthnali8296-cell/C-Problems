080ee0bc83c39365ed59995f9a14c6d731b9dfb8
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-06-11 20:38:05 +0530

    Add files via upload

---
+++ c program/pp.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int A=100;
+	int B=100;
+	if(A>B){
+	
+	     printf("A is Grater Than B :A=%d,B=%d ",A,B);
+	}else if(A=B){
+	
+	printf("A & B are Equal:A=%d,B=%d ",A,B);
+	}else
+	printf("B is Grater Than A: A=%d,B=%d ",A,B);	
+	
+}
