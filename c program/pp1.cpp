e6b81ce7a80f6441eb734e9e35fd98ee9b82e8f2
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-06-12 20:25:24 +0530

    Add files via upload

---
+++ c program/pp1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("ENTER YOUR MARKS:");
+	scanf("%d",&a);
+	if(a>90)
+	{
+		printf("CGPA =9");
+	}	else if(a>70&&a<90)
+		printf("CGPA A=8");
+		else if(a>50&&a<70)
+		printf("CGPA = 6");
+		else
+		printf("GOVINDA GOOOVINDA");
+	
+	
+}
