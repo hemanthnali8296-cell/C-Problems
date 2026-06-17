bf1d3e1299636e105b474c06aa2eeac63aaf3a97
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-06-17 14:56:27 +0530

    Add files via upload

---
+++ c problems/division(quotient&reminder).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int q = a / b;
+    int r = a % b;
+
+    printf("%d %d", q, r);
+
+    return 0;
+}
+                                                                                                                                                                               
+t
