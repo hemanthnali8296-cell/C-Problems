5d9bd74d5528fb6a40a3a3f77a8d705dd8ae7377
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-07-08 21:53:16 +0530

    Add files via upload

---
+++ c program/pp16.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+	printf("%d Min Value of INTEGER is:\n ",INT_MIN);
+	printf("%d Maximum Value of INTEGER is: \n",INT_MAX);
+	printf("%d Min Value of CHARACTER is:\n ",CHAR_MIN);
+	printf("%d Maximum Value of CHARACTER is: \n",CHAR_MAX);
+}
