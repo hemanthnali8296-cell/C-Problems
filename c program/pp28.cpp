efa644024a4688f55a510b8b6e05c675bcc58fdc
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-07-24 20:03:31 +0530

    Add files via upload

---
+++ c program/pp28.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+int main(){
+ char name[54]="Sri Shanmugha College of Engineering And Technology";
+ printf("%s\n\n",name);
+ printf("%-10.5s\n\n",name);
+}
