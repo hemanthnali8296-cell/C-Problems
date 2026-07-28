15e719b8d8ff462fda536a6b3f179ed1406e9772
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-07-28 20:08:02 +0530

    Add files via upload

---
+++ c program/pp30.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main(){
+	int i;
+	char name[12]="ENGINEERING";
+	//int length =strlen(name);
+	for( i=0;i<10;i++){
+	
+	putchar(name[i]);
+	printf("\n");
+}
+}
