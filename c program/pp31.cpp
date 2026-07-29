4a2f0274679dfd703d5e309c62d97667a338d666
hemanthnali8296-cell <hemanthnali8296@gmail.com>
2026-07-29 21:22:09 +0530

    Add files via upload

---
+++ c program/pp31.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main (){
+	char x='a';
+	char name1[20]="Nali Hemanth Kumar";
+	printf("The Length Of %s is:%d Charecters\n",name1,strlen(name1));
+		char name2[20]="Koliki madhan kumar";
+	printf("The Length Of %s is:%d Charecters\n",name2,strlen(name2));
+		char name3[20]="Koliki Madhava Rao";
+	printf("The Length Of %s is:%d Charecters\n",name3,strlen(name3));
+		char name4[20]="Nandha Kishore";
+	printf("The Length Of %s is:%d Charecters\n",name4,strlen(name4));
+		char name5[20]="Mukesh Yadav";
+	printf("The Length Of %s is:%d Charecters\n",name5,strlen(name5));
+		char name6[20]="Mani Kowshik";
+	printf("The Length Of %s is:%d Charecters\n",name6,strlen(name6));
+		char name7[20]="Deepu";
+	printf("The Length Of %s is:%d Charecters\n",name7,strlen(name7));
+		char name8[20]="Karthik";
+	printf("The Length Of %s is:%d Charecters\n",name8,strlen(name8));
+		char name9[20]="Praveen";
+	printf("The Length Of %s is:%d Charecters\n",name9,strlen(name9));
+		char name10[20]="Praveen";
+	printf("The Length Of %s is:%d Charecters\n",name10,strlen(name10));
+}
